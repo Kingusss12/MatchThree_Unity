7ac6c1ffd2e27fdf95c4ed83245a8edeab420552
Kingusss12 <kinga12nagy@gmail.com>
2018-11-18 13:07:57 +0100

    Measuring time of CountScoreRoutine

--- Assets/Scripts/Singletons/Managers/ScoreManager.cs
+++ Assets/Scripts/Singletons/Managers/ScoreManager.cs
@@ -6,6 +6,10 @@ public class ScoreManager : Singleton<ScoreManager>
 {
     public int CurrentScore { get; private set; }
 
+    int numRecordings = 100; // number of recordings to make
+    float timeRecorded = 0;  // total accumulated time
+    int recordingNum = 0;    // current recording number
+
     [SerializeField] private Text _scoreText;
 
     private int _increment = 1;
@@ -37,9 +41,31 @@ public class ScoreManager : Singleton<ScoreManager>
     ///<summary> Adds value to score</summary>
     public void AddScore(int value)
 	{
-		CurrentScore += value;
-		StartCoroutine (CountScoreRoutine ());
-	}
+        CurrentScore += value;
+
+
+        //Counting time to execute CountScoreRoutine
+
+        float startTime = Time.realtimeSinceStartup;
+
+        StartCoroutine(CountScoreRoutine());
+       
+        float endTime = Time.realtimeSinceStartup;
+        float finalTime = (endTime - startTime);
+        recordingNum++;
+        timeRecorded += finalTime;
+        if (recordingNum == numRecordings)
+        {
+
+            // calculate and display the average time
+            float averageTime = timeRecorded / numRecordings;
+            Debug.Log("Avarage time: " + averageTime + " seconds");
+
+            recordingNum = 0;
+            timeRecorded = 0;
+
+    }
+    }
 
     ///<summary> Increments score gradually</summary>
     IEnumerator CountScoreRoutine()
@@ -47,15 +73,17 @@ public class ScoreManager : Singleton<ScoreManager>
 		int iterations = 0;
         int counterValue = GetScoreText();
 
-        while (counterValue < CurrentScore && iterations < 100000) 
-		{
-			counterValue += _increment;
-			UpdateScoreText (counterValue);
-			iterations++;
-			yield return null;
-		}
+        while (counterValue < CurrentScore && iterations < 100000)
+        {
+            counterValue += _increment;
+            UpdateScoreText(counterValue);
+            iterations++;
+            yield return null;
+        }
 
-		counterValue = CurrentScore;
+        counterValue = CurrentScore;
 		UpdateScoreText (CurrentScore);
-	}
+//      yield return null;
+
+    }
 }
